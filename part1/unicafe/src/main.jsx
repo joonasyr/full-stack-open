8f3a67ff8ac0ef4849050292c4095b1d11674e06
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-06 14:49:42 +0300

    1.6 unicafes step1 done

---
+++ part1/unicafe/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
