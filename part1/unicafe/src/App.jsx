bc708424f79e697072bc59bc732105b471b1d2db
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-06 17:19:17 +0300

    1.9 unicafe step4 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,13 +8,13 @@ const App = () => {
 
 	const handleClick = (type) => {
 		setAll(all + 1)
-		
+
 		if (type === 'good') {
 			setGood(good + 1)
 		} else if (type === 'neutral') {
-				setNeutral(neutral + 1)
+			setNeutral(neutral + 1)
 		} else if (type === 'bad') {
-				setBad(bad + 1)
+			setBad(bad + 1)
 		}
 	}
 
@@ -24,23 +24,27 @@ const App = () => {
 			<button onClick={() => handleClick('good')}>good</button>
 			<button onClick={() => handleClick('neutral')}>neutral</button>
 			<button onClick={() => handleClick('bad')}>bad</button>
-			<h2>statistics</h2>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {all}</p>
-			<Statistics all={all} good={good} bad={bad} />
+			<Statistics all={all} good={good} neutral={neutral} bad={bad} />
 		</>
 	)
 }
 
 export default App
 
-const Statistics = ({all, good, bad}) => {
+const Statistics = ({all, good, neutral, bad}) => {
 	return (
 		<>
-			<p>average {all > 0 ? (good - bad) / all : 0}</p>
-			<p>positive {all > 0 ? (good) / all * 100 : 0} %</p>
+		{all > 0 ?
+			<>
+				<h2>statistics</h2>
+				<p>good {good}</p>
+				<p>neutral {neutral}</p>
+				<p>bad {bad}</p>
+				<p>all {all}</p>
+				<p>average {(good - bad) / all}</p>
+				<p>positive {(good) / all * 100} %</p>
+			</>
+			: <p>no feedback given</p>}
 		</>
 	)
 }
