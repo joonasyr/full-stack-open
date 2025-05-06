9767878820b67432e140adea98ad091e50cf7a57
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-06 17:02:17 +0300

    1.7 unicafe step 2 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,33 @@ const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
+	const [all, setAll] = useState(0)
+
+	const handleClick = (type) => {
+		setAll(all + 1)
+		
+		if (type === 'good') {
+			setGood(good + 1)
+		} else if (type === 'neutral') {
+				setNeutral(neutral + 1)
+		} else if (type === 'bad') {
+				setBad(bad + 1)
+		}
+	}
 
 	return (
 		<>
 			<h2>give feedback</h2>
-			<button onClick={() => setGood(good + 1)}>good</button>
-			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-			<button onClick={() => setBad(bad + 1)}>bad</button>
+			<button onClick={() => handleClick('good')}>good</button>
+			<button onClick={() => handleClick('neutral')}>neutral</button>
+			<button onClick={() => handleClick('bad')}>bad</button>
 			<h2>statistics</h2>
 			<p>good {good}</p>
 			<p>neutral {neutral}</p>
 			<p>bad {bad}</p>
+			<p>all {all}</p>
+			<p>average {all > 0 ? (good - bad) / all : 0}</p>
+			<p>positive {all > 0 ? (good) / all * 100 : 0} %</p>
 		</>
 	)
 }
