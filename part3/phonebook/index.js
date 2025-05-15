31d70c5bece761505d47e3b03fe13dfd6cd74776
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-15 14:03:05 +0300

    3.7 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -93,4 +95,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 const PORT = 3001
-app.listen(PORT, () => `Server listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
+
