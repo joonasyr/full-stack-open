184b7f23ce302d2b0a9de69919367dd1db37ada4
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-16 12:46:01 +0300

    3.11 done

--- part3/phonebook-frontend/src/services/personService.tsx
+++ part3/phonebook-frontend/src/services/personService.tsx
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 
 const create = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data)
-
 const update = (updatedPerson) => {}
 
 const deletePerson = (id) => axios.delete(`${baseUrl}/${id}`).then(res => res.data)
