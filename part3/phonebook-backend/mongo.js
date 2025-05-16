efb90bcb09cc0ca0e87074cd28574c035cad9b0d
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-16 14:11:26 +0300

    3.12 done

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length !== 3 && process.argv.length !== 5) {
+  console.log('Incorrect arguments')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const dbUrl = `mongodb+srv://joonasyr:${password}@cluster0.uiceotc.mongodb.net/phonebook?retryWrites=true&w=majority&appName=cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(dbUrl)
+
+const personSchema = new mongoose.Schema ({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const person = new Person({
+    name: name,
+    number: number 
+  })
+
+  person.save().then(result => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
