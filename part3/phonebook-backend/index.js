cf078c21b6f70f7a3897a3dbe4702170a4aa5245
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-05-16 12:41:00 +0300

    3.11 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(cors())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
     { 
