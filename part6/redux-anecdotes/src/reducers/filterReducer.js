b41f88c1732bb16480392d12fe86259b39e560c8
Joona Syrjämäki <joona.syrjamaki@gmail.com>
2025-12-18 16:39:48 +0200

    6.10 done

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,15 +1,14 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default:
-      return state
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-export const setFilter = (value) => ({
-  type: 'SET_FILTER',
-  payload: value,
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(_state, action) {
+      return action.payload
+    },
+  },
 })
 
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
